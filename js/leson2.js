719744595e1556be4eac6be726073a67680764ad
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-03-18 12:39:02 +0200

    task6

--- js/leson2.js
+++ js/leson2.js
@@ -125,3 +125,28 @@
 //     console.log(`${key} : ${user[key]}`)
 //     // console.log(user[key]);
 //  }
+
+// -------------------------------------------------
+
+//3. Напишіть ф-цію calcTotalPrice(stones, stonesName),
+//яка приймає массив об'єктів і
+//рядок з назвами каміння.
+//Функція рахує і повертає загальну вартість каменів
+//з таким ім'ям, ціною та кількістю з об'єкта
+
+const stones = [
+  { name: "Смарагд", price: 1300, quantity: 4 },
+  { name: "Діамант", price: 2700, quantity: 6 },
+  { name: "Сапфір", price: 400, quantity: 7 },
+  { name: "Щебінь", price: 150, quantity: 100 },
+];
+function calcTotalPrice(stones, stonesName) {
+    for (const stone of stones) {
+        if (stone.name === stonesName) {
+            return stone.price * stone.quantity;
+        }
+        
+    }
+}
+
+console.log(calcTotalPrice(stones, "Сапфір"));
