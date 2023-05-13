7364168bc6e4c3be5b7af5f7190bfdc6add4ed60
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-05-13 11:02:11 +0200

    comit

---
+++ js/leson6.js
@@ -0,0 +1,48 @@
+// let i = 0;
+// setTimeout(() => alert(i), 100);
+
+// for (let j = 0; j < 1000000000; j++) {
+//   i++;
+// }
+
+// =================================================
+
+// const first = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 500, "один");
+// });
+
+// const second = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, "два");
+// });
+
+// Promise.race([first, second]).then((res) => console.log(res));
+
+// =================================================
+
+// setTimeout(() => console.log(1), 0);
+
+// console.log(2);
+
+// new Promise((res) => {
+//   console.log(3);
+//   res();
+// }).then(() => console.log(4));
+
+// console.log(5);
+
+// ================================================
+
+// Перероби функцію на проміс таким чином, щоб проміс повертав значення
+// через 2 секунди після виклику функції
+
+// function greet() {
+//   return "hello world";
+// }
+
+// const greetPromise = new Promise((res) => {
+//     setTimeout(() => {
+//     res("hello world");
+//     },2000)
+// })
+
+// greetPromise.then((resp) => console.log(resp))
